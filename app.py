da912874f98ef95ae8b9a9fab60c9726b3622610
NathanJargon <metamorphiaSH@gmail.com>
2023-10-09 15:46:02 +0800

    Added Bootstrap. Added sidebar. Added CSV-feature.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+import csv
+from io import StringIO
 
 app = Flask(__name__)
 
@@ -48,6 +50,37 @@ def index():
         return redirect(url_for('index'))
 
     return render_template('index.html', expenses=fetch_expenses())
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_csv():
+    if request.method == 'POST':
+        csv_file = request.files['csv_file']
+
+        if csv_file:
+            # Read CSV file and parse it
+            csv_data = csv_file.read().decode('utf-8')
+            csv_reader = csv.reader(StringIO(csv_data))
+
+            # Skip the header row if it exists
+            next(csv_reader, None)
+
+            conn = sqlite3.connect('expenses.db')
+            cursor = conn.cursor()
+
+            for row in csv_reader:
+                if len(row) == 4:
+                    description, amount, date, category = row
+                    cursor.execute('''
+                        INSERT INTO expenses (description, amount, date, category)
+                        VALUES (?, ?, ?, ?)
+                    ''', (description, float(amount), date, category))
+
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('index'))
+
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
