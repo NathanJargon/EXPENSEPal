85779d6612ede0040fa1f5c8b4c8a62dcec8b3fd
NathanJargon <metamorphiaSH@gmail.com>
2023-10-19 17:06:31 +0800

    yes

--- app.py
+++ app.py
@@ -1,11 +1,88 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import csv
 from io import StringIO
+=======
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, make_response, jsonify
+import sqlite3
+import csv
+from io import StringIO
+from waitress import serve
+import firebase_admin
+from firebase_admin import credentials, auth, db, firestore
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 
+<<<<<<< Updated upstream
+=======
+
+# Initialize the Firebase Admin SDK with your project credentials
+cred = credentials.Certificate('firebase-admin.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# Function to get the Firebase user ID based on the Firebase ID token
+def get_firebase_user_id(firebase_id_token):
+    try:
+        decoded_token = auth.verify_id_token(firebase_id_token)
+        return decoded_token.get('uid')
+    except Exception as e:
+        # Handle any exceptions that might occur during Firebase Authentication verification
+        print(f"Firebase Authentication error: {str(e)}")
+        return None
+
+
+@app.route('/add_expense', methods=['POST'])
+def add_expense():
+    # Access the JSON data sent with the POST request
+    data = request.get_json()
+
+    # Get the Firebase user ID for the currently logged-in user
+    firebase_id_token = request.headers.get('Authorization').split('Bearer ')[-1]
+
+    user_id = get_firebase_user_id(firebase_id_token)
+    
+    if 'uid' in data:
+        # Associate the user ID with the expense data
+        uid = data['uid']
+        # Check if the user is authorized
+        # Add the expense data to Firestore
+        expenses_ref = db.collection('expenses')
+        expenses_ref.add(data)
+
+        response_data = {'message': 'Expense added successfully', 'data': data}
+        return jsonify(response_data)
+    else:
+        return "Unauthorized access"
+    
+
+@app.route('/view_expenses')
+def view_expenses():
+    # Get the Firebase user ID for the currently logged-in user
+    firebase_id_token = request.cookies.get('your_firebase_cookie_name')
+    user_id = get_firebase_user_id(firebase_id_token)
+
+    if user_id:
+        # Fetch and display expenses for the user
+        conn = sqlite3.connect('expenses.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM expenses WHERE user_id = ?', (user_id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return render_template('expenses.html', expenses=rows)
+    else:
+        # Handle unauthorized access by returning a plain text response
+        return "Unauthorized access"
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+    
+>>>>>>> Stashed changes
 def create_database():
     conn = sqlite3.connect('expenses.db')
     cursor = conn.cursor()
@@ -84,6 +161,7 @@ def upload_csv():
     return render_template('upload.html')
 
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     app.run(debug=True)
 =======
 from flask import Flask, render_template, request, redirect, url_for
@@ -140,3 +218,6 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 >>>>>>> 4fa81673175f33b42d6969f4e82bb48b134094f7
+=======
+    app.run(debug=True)
+>>>>>>> Stashed changes
