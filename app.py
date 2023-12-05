32c169fbe3e0181884e22221eb8c109ecc42f446
NathanJargon <metamorphiaSH@gmail.com>
2023-12-06 00:10:17 +0800

    improved!

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, make_response, jsonify
-import sqlite3
-import csv
+import sqlite3, csv, time
 from io import StringIO
 from waitress import serve
 import firebase_admin
@@ -8,70 +7,137 @@ from firebase_admin import credentials, auth, db, firestore
 
 app = Flask(__name__, static_url_path='/static')
 
-
-# Initialize the Firebase Admin SDK with your project credentials
 cred = credentials.Certificate('firebase-admin.json')
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
-# Function to get the Firebase user ID based on the Firebase ID token
 def get_firebase_user_id(firebase_id_token):
     try:
         decoded_token = auth.verify_id_token(firebase_id_token)
         return decoded_token.get('uid')
     except Exception as e:
-        # Handle any exceptions that might occur during Firebase Authentication verification
         print(f"Firebase Authentication error: {str(e)}")
         return None
 
+def verify_id_token(id_token):
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        # Check if the token is not expired
+        if decoded_token['exp'] >= time.time():
+            return decoded_token
+        else:
+            return None
+    except auth.ExpiredIdTokenError:
+        # Handle token expiration error
+        print("Token has expired.")
+        return "Expired"
+    except Exception as e:
+        # Handle other errors
+        print("Error verifying token:", str(e))
+        return "Error"
+    
+
+@app.route('/get_expenses', methods=['GET'])
+def get_expenses():
+    # Initialize the Firestore client
+    db = firestore.client()
+
+    # Replace 'expenses' with the name of your Firestore collection
+    expenses_ref = db.collection('expenses')
+
+    # Fetch all expenses from the Firestore collection
+    expenses = expenses_ref.stream()
+
+    # Convert expenses to a list of dictionaries
+    expenses_data = []
+
+    for expense in expenses:
+        expense_data = expense.to_dict()
+        expenses_data.append(expense_data)
+
+    return jsonify({'expenses': expenses_data})
+
+from flask import jsonify
 
 @app.route('/add_expense', methods=['POST'])
 def add_expense():
-    # Access the JSON data sent with the POST request
     data = request.get_json()
+    firebase_id_token = request.headers.get('Authorization')
 
-    # Get the Firebase user ID for the currently logged-in user
-    firebase_id_token = request.headers.get('Authorization').split('Bearer ')[-1]
-    user_id = get_firebase_user_id(firebase_id_token)
-    
-    if 'uid' in data:
-        # Associate the user ID with the expense data
-        uid = data['uid']
-        # Check if the user is authorized
-        # Add the expense data to Firestore
-        expenses_ref = db.collection('expenses')
-        expenses_ref.add(data)
+    print('Received Firebase ID token:', firebase_id_token)
+    print('Received expense data:', data)
 
-        response_data = {'message': 'Expense added successfully', 'data': data}
-        return jsonify(response_data)
+    if firebase_id_token:
+        firebase_id_token = firebase_id_token.split('Bearer ')[-1]
+
+        # Log the received Firebase ID token
+        print('Received Firebase ID token:', firebase_id_token)
+
+        user_id = verify_id_token(firebase_id_token)  # Verify the ID token
+
+        if user_id:
+            if isinstance(user_id, dict) and 'user_id' in user_id:
+                # Add the expense data to Firestore
+                expenses_ref = db.collection('expenses')
+                result = expenses_ref.add(data)
+
+                # Get the auto-generated ID
+                auto_generated_id = result[1].id
+
+                # Use 'user_id' for the actual user identifier
+                data['user_id'] = user_id['user_id']
+                
+                # Use 'data_id' for the auto-generated ID
+                data['data_id'] = auto_generated_id
+
+                response_data = {
+                    'message': 'Expense added successfully',
+                    'data': data,
+                    'data_id': auto_generated_id  # Include the ID in the response
+                }
+
+                # Replace the 'user_id' and 'data_id' fields in Firestore
+                expenses_ref.document(auto_generated_id).set({
+                    'user_id': user_id['user_id'],
+                    'data_id': auto_generated_id
+                    # Add other fields as needed
+                }, merge=True)
+
+                return jsonify(response_data)
+            else:
+                return jsonify({'message': 'Invalid user ID in the token'})
+
+        else:
+            return jsonify({'message': 'Unauthorized access'})
     else:
-        return "Unauthorized access"
-    
+        return jsonify({'message': 'Missing Authorization header'})
+
 
 @app.route('/view_expenses')
 def view_expenses():
     # Get the Firebase user ID for the currently logged-in user
-    firebase_id_token = request.cookies.get('your_firebase_cookie_name')
+    firebase_id_token = request.cookies.get('your_cookie_name')
     user_id = get_firebase_user_id(firebase_id_token)
+    print(firebase_id_token)
+    print(user_id)
 
     if user_id:
-        # Fetch and display expenses for the user
-        conn = sqlite3.connect('expenses.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM expenses WHERE user_id = ?', (user_id,))
-        rows = cursor.fetchall()
-        conn.close()
-        return render_template('expenses.html', expenses=rows)
+        # Fetch and display expenses for the user from Firestore
+        expenses_ref = db.collection('expenses').where('user_id', '==', user_id)
+        expenses = expenses_ref.stream()
+        expenses_data = [expense.to_dict() for expense in expenses]
+
+        return render_template('expenses.html', expenses=expenses_data)
     else:
-        # Handle unauthorized access by returning a plain text response
+        # Handle unauthorized access by returning a plain text response or redirecting to the login page.
         return "Unauthorized access"
+
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
     
-def create_database():
+"""def create_database():
     conn = sqlite3.connect('expenses.db')
     cursor = conn.cursor()  
     cursor.execute('''
@@ -95,22 +161,32 @@ def fetch_expenses():
     cursor.execute('SELECT * FROM expenses')
     rows = cursor.fetchall()
     conn.close()
-    return rows
+    return rows"""
 
-@app.route('/delete_expense', methods=['POST'])
-def delete_expense():
-    if request.method == 'POST':
-        expense_id = request.form['expense_id']
 
-        conn = sqlite3.connect('expenses.db')
-        cursor = conn.cursor()
+@app.route('/delete_expense/<expense_uid>', methods=['DELETE'])
+def delete_expense(expense_uid):
+    print("Received expense_uid:", expense_uid)
+    try:
+        # Initialize the Firestore client
+        db = firestore.client()
 
-        # Delete the expense with the given ID
-        cursor.execute('DELETE FROM expenses WHERE id = ?', (expense_id,))
-        conn.commit()
-        conn.close()
+        # Replace 'expenses' with the name of your Firestore collection
+        expenses_ref = db.collection('expenses')
+        print("Attempting to delete document:", expense_uid)
 
-    return redirect(url_for('index'))
+        # Delete the expense with the given expense_uid
+        result = expenses_ref.document(expense_uid).delete()
+        if result is not None:
+            print("Expense deleted successfully:", result)
+            return jsonify({'message': 'Expense deleted successfully'})
+        else:
+            print("Expense not found or unauthorized access")
+            return jsonify({'message': 'Expense not found or unauthorized access'}, 404)
+    except Exception as e:
+        print(f'Error deleting expense: {str(e)}')
+        return jsonify({'message': f'Error deleting expense: {str(e)}'}, 500)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -131,7 +207,7 @@ def index():
 
         return redirect(url_for('index'))
 
-    return render_template('index.html', expenses=fetch_expenses())
+    return render_template('index.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_csv():
--- static/dist/logoutOverlay.js
+++ static/dist/logoutOverlay.js
@@ -1,45 +1,51 @@
 const logoutButton = document.getElementById('loginLogout');
 const logout_textContent = document.getElementById('loginLogoutLink');
 const userNameElement = document.getElementById('userName');
-let timer;
+
+// Function to set the user name in localStorage
+function setUserNameInLocalStorage(userName) {
+    localStorage.setItem('userName', userName);
+}
+
 // Function to clear the userName from localStorage
 function clearUserNameFromLocalStorage() {
     localStorage.removeItem('userName');
     userNameElement.textContent = 'User';
-    loginLogoutLink.textContent = 'Login';
+    logout_textContent.textContent = 'Login';
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log('User logged out');
-        loginLogoutLink.textContent = 'Login';
+        logout_textContent.textContent = 'Login';
         userNameElement.textContent = 'User';
         // Optionally, you can redirect the user to a login page or perform other actions.
     }).catch((error) => {
         // An error occurred during sign-out.
         console.error('Sign-out error:', error);
     });
+}
+
+// Check if the user name is stored in localStorage
+const storedUserName = localStorage.getItem('userName');
+
+if (storedUserName) {
+    // If a user name is found, set it and show "Logout" instead of "Login"
+    userNameElement.textContent = storedUserName;
+    logout_textContent.textContent = 'Logout';
+    startLogoutTimer();
+} else {
+    // If no user name is found, show "Login"
+    logout_textContent.textContent = 'Login';
 }
 
 // Start the timer when the user logs in
 function startLogoutTimer() {
-    timer = setTimeout(() => {
-      clearUserNameFromLocalStorage();
+    setTimeout(() => {
+        clearUserNameFromLocalStorage();
     }, 20 * 60 * 1000); // 20 minutes in milliseconds
 }
 
-a = setTimeout(clearUserNameFromLocalStorage, 20 * 60 * 1000);
 logoutButton.addEventListener('click', () => {
-    if(logout_textContent.textContent === 'Logout') {
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log('User logged out');
-        localStorage.removeItem('userName');
-        loginLogoutLink.textContent = 'Login';
-        userNameElement.textContent = 'User';
-        // Optionally, you can redirect the user to a login page or perform other actions.
-    }).catch((error) => {
-        // An error occurred during sign-out.
-        console.error('Sign-out error:', error);
-    });
-}});
-
-startLogoutTimer();
+    if (logout_textContent.textContent === 'Logout') {
+        clearUserNameFromLocalStorage();
+    }
+});
